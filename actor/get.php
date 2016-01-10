928dde4322df8de94a44a26e6d6e7a7c1d400709
Milo Cesar <milocesar1@gmail.com>
2016-01-10 15:08:32 +0100

    Get Actors

--- actor/get.php
+++ actor/get.php
@@ -5,6 +5,8 @@ require('../utilities/database.php');
 if(!empty($_GET['id'])){
     echo('<H1>'.$_GET['íd'].'</H1>');
     echo('TRUE');
+
+    query("SELECT * FROM Actors");
 } else {
     echo("No id");
 }
