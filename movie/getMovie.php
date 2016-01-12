23b77f1ed91dcd828e82c176aaac0d5db3d93275
Milo Cesar <milocesar1@gmail.com>
2016-01-12 21:11:49 +0100

    Alingment

--- movie/getMovie.php
+++ movie/getMovie.php
@@ -62,7 +62,7 @@ if(!empty($_GET['id'])){
                 }
 
                 while($movie = mysqli_fetch_array($movieObj)){
-                    showInfoForMovie($movie);
+                    showInfoForMovie($movie, 6, 3);
                     echo '<div class="col-md-1"></div>';
                 }
     echo '  </div>
