94624dea084fbcdca5abf2cf8e190a1623da44fb
Milo Cesar <milocesar1@gmail.com>
2016-01-12 20:01:56 +0100

    Actor Formatting

--- actor/getActor.php
+++ actor/getActor.php
@@ -37,18 +37,27 @@ if(!empty($_GET['id'])){
 
 
 } else {
-    $actorObj = getRandomActors(4);
+    echo'
+    <div class="col-md-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Featured Actors</h3>
+            </div>
+            <div class="panel-body">';
+                $actorObj = getRandomActors(4);
 
-    if ($actorObj == false){
-        return $actorObj;
-    }
+                if ($actorObj == false){
+                    return $actorObj;
+                }
 
-    echo '<div class="col-md-12">';
-    while($actor = mysqli_fetch_array($actorObj)){
-        showInfoForActor($actor);
-        echo '<div class="col-md-1"></div>';
-    }
-    echo '</div>';
+                while($actor = mysqli_fetch_array($actorObj)){
+                    showInfoForActor($actor);
+                    echo '<div class="col-md-1"></div>';
+                }
+    echo '  </div>
+        </div>
+    </div>
+    ';
 
 }
 
--- utilities/templates/actorTemplate.php
+++ utilities/templates/actorTemplate.php
@@ -4,25 +4,25 @@ function showInfoForActor($actor){
 	$firstName = $actor['firstname'];
 	$lastName = $actor['lastname'];
 	echo'
-	<div class="col-md-3 lib-item" data-category="view">
-		<a href="/actor/getActor.php?id='.$id.'">
-		    <div class="lib-panel">
-		        <div class="row box-shadow">
-		            <div class="col-md-6">
-		                <img class="lib-img-show" src="/actor/img/'.$id.'.jpg">
-		            </div>
-		            <div class="col-md-6">
+	<div class="col-md-2 lib-item" data-category="view">
+	    <div class="lib-panel">
+	        <div class="row box-shadow">
+	            <div class="col-md-6">
+	                <img class="lib-img-show" src="/actor/img/'.$id.'.jpg">
+	            </div>
+	            <div class="col-md-6">
+	                <a href="/actor/getActor.php?id='.$id.'" >
 		                <div class="lib-row lib-header">
 		                    '.$firstName.' '.$lastName.'
 		                    <div class="lib-header-seperator"></div>
 		                </div>
-		                <div class="lib-row lib-desc">
-		                    Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor
-		                </div>
-		            </div>
-		        </div>
-		    </div>
-		</a>
+		            </a>
+	                <div class="lib-row lib-desc">
+	                    Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor
+	                </div>
+	            </div>
+	        </div>
+	    </div>
 	</div>';
 }
 ?>
